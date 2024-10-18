ecbf11790728f37405984677fb9ded1b0b5a66dd
Daniel Depoe <89668578+dan-montelo@users.noreply.github.com>
2024-10-18 13:18:14 -0400

    update

--- nf-sample.py
+++ nf-sample.py
@@ -1,9 +1,20 @@
 import rhinoscriptsyntax as rs
 
-nf = rs.GetPlugInObject('NorhternFlicker')
+#this is the key- currently this prints None.
+nf = rs.GetPlugInObject('NorthernFlicker')
+print (nf)
+#print results
+# #None
+
+#with cadwork tools plugin loaded.
+cw = rs.GetPlugInObject('CadworkTools')
+print (cw)
+#print results
+#<CadworkTools.CadworkToolsScriptAccess object at 0x000000000000012A [CadworkTools.CadworkToolsScriptAccess]>
 
 objs =rs.GetObjects("Pick a brep.", 16, True, False, False)
 
+#if nf returned a NorthernFlicker.NorthernFlickerScriptAccess object at ....... then this would work:
 for obj in objs:
     Width = nf.Width
     Depth = nf.Height
